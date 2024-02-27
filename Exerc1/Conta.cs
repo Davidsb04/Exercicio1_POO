e09197cf59dbce301f18f7a28da55ef331e0d5e5
Davidsb04 <121972361+Davidsb04@users.noreply.github.com>
2024-02-27 15:58:14 -0300

    Adicionar arquivos de projeto.

---
+++ Exerc1/Conta.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exerc1
+{
+    internal class Conta
+    {
+        private Stack<string> extrato = new Stack<string>(10);
+        private double saldo;
+        private int identificador;
+        private string CPF;
+
+        public Conta(string cpf, int identificador)
+        {
+            saldo = 0;
+            CPF = cpf;
+            this.identificador = identificador;
+        }
+
+        public void DepositarValor()
+        {
+            Console.Write("\nDigite o valor que deseja depositar: R$");
+            double valor = double.Parse(Console.ReadLine());
+
+            if (saldo < 0)
+            {
+                saldo += CalcularTaxaDeDeposito(valor);
+                Console.WriteLine("\nDeposito com taxa de 3% realizado com sucesso!");
+                extrato.Push($"Deposito: R${valor}");
+            }
+            else
+            {
+                saldo += valor;
+                Console.WriteLine("\nDeposito realizado com sucesso!");
+                extrato.Push($"Deposito: R${valor}");
+            }
+        }
+        public void SacarValor()
+        {
+            Console.Write("\nDigite o valor que deseja sacar: R$");
+            double valor = double.Parse(Console.ReadLine());
+
+            double max_Saque = saldo + 100;
+
+            if(valor > max_Saque)
+            {
+                Console.WriteLine("\nEsse valor ultrapassa o limite para o saque!");
+            }
+            else
+            {
+                saldo -= valor;
+                Console.WriteLine("\nSaque realizado com sucesso!");
+                extrato.Push($"Saque: R${valor}");
+            }
+        }
+        public void ExibirSaldo()
+        {
+            if(saldo > 0)
+            {
+                Console.WriteLine($"\nO saldo positivo da conta é R${saldo}");
+            }
+            else
+            {
+                Console.WriteLine($"\nO saldo negativo da conta é R${saldo}");
+            }
+        }
+        public double CalcularTaxaDeDeposito(double valor)
+        {
+            double taxa = saldo * 0.03;
+
+            return valor += taxa;
+        }
+        
+        public void ExibirExtrato()
+        {
+            if(extrato.Count == 0)
+            {
+                Console.WriteLine("\nNão foram feitas transações na conta.");
+            }
+            else
+            {
+                string elementoDoExtrato = extrato.Pop();
+                Console.WriteLine(elementoDoExtrato);
+            }
+        }
+    }
+}
---
+++ Exerc1/Program.cs
@@ -0,0 +1,57 @@
+﻿using System.Security.Cryptography;
+
+namespace Exerc1
+{
+    internal class Program
+    {
+        static int opcao = 0;
+
+        static void Main(string[] args)
+        {
+            adicionarConta();
+        }
+
+        static void adicionarConta()
+        {
+            Console.Write("\nInforme seu CPF: ");
+            string cpf = Console.ReadLine();
+
+            Random random = new Random();
+            int identificador = random.Next(10000, 100000);
+            Console.WriteLine("\nO identificador da sua conta é: " + identificador);
+
+            Conta conta = new Conta(cpf, identificador);
+
+            operacoesDaConta(opcao, conta);
+        }
+        static void operacoesDaConta(int opcao, Conta conta)
+        {
+            do
+            {
+                Console.Write("\n[1] Depositar valor \n[2] Sacar valor \n[3] Exibir saldo \n[4] Exibir extrato \n[5] Sair \n\nEscolha uma opção: ");
+                opcao = int.Parse(Console.ReadLine());
+
+                switch (opcao)
+                {
+                    case 1:
+                        conta.DepositarValor();
+                        break;
+                    case 2:
+                        conta.SacarValor();
+                        break;
+                    case 3:
+                        conta.ExibirSaldo();
+                        break;
+                    case 4:
+                        conta.ExibirExtrato();
+                        break;
+                    case 5:
+                        break;
+                    default:
+                        Console.WriteLine("\nOpção inválida!");
+                        break;
+                }
+            } while (opcao != 5);
+        }
+    }    
+}
